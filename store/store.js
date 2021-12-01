6827c93a547e222ffd25110efe6f9e3b81d10a6e
kelzamoth <kelzamoth@gmail.com>
2021-12-01 23:00:53 +0300

    Update store.js

--- store/store.js
+++ store/store.js
@@ -5,7 +5,7 @@ import rootReducer from "../src/reducers/index";
 import { createLogger } from "redux-logger";
 
 const persistConfig = {
-  key: "redux-taskkk",
+  key: "redux-taskkkk",
   storage: AsyncStorage,
   blacklist: [],
 };
